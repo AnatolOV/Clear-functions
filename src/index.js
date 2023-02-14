d0e3a2b67cf1516864f37db6cf00c2cad83546b8
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2023-02-14 19:12:28 +0300

    e1

---
+++ src/index.js
@@ -0,0 +1,13 @@
+export function healthRate(object) {
+  if (object.health > 50) {
+    console.log(`${object.name} healthy`);
+    return "healthy";
+  } else if (object.health <= 50 && object.health > 15) {
+    console.log(`${object.name} wounded`);
+    return "wounded";
+  } else {
+    console.log(`${object.name} critical`);
+    return "critical";
+  }
+}
+healthRate({ name: "Маг", health: 15 });
--- test/test.js
+++
@@ -1,6 +0,0 @@
-const { healthRate } = require('../index.js');
-
-test('basic test', ()=> {
-    let result = 4;   
-    expect(resullt).toBe(4),
-});
